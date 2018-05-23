06f5f5034bdb6934b3be370e07ef86b8f8bb897f
BIOCOSM <biocosm@protonmail.com>
2018-05-23 11:32:32 -0600

    added netlify-CMS dependencies and basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
     }
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-next`,
     // `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
     // 'gatsby-plugin-styled-jsx-postcss', // as above
